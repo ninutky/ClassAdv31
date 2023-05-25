fc56e557d7eed6d642b4b33aa95f2723d21ed109
Nagyeong Park <zzum0701@naver.com>
2023-05-25 14:45:51 +0900

    속성(property) 실습

---
+++ ClassAdv31/Box.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ClassAdv31
+{
+    internal class Box
+    {
+        private int width;
+
+        public int Width
+        {
+            get { return width; }
+            set { 
+                if(value<= 0)
+                {
+                    throw (new Exception("width는 양수여야 합니다."));
+                }
+                width = value;
+            }
+        }
+        
+        private int height;
+
+        public int Height
+        {
+            get { return height; }
+            set { height = value; }
+        }
+
+        public Box(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+
+    }
+}
